07457d2c99169b42e03180bea212e9caeec6e375
Sharon <sharonvandergeest@live.nl>
2016-07-01 10:19:39 +0200

    Initial commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+sass = require('node-sass');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +14,16 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// adding the sass middleware
+app.use(
+ sass.middleware({
+   src: __dirname + '/stylesheets/sass', 
+   dest: __dirname + '/public',
+   prefix:  '/stylesheets',
+   debug: true,       
+ })
+ );   
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
