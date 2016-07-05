65a63d74ffa7b1f7b5341bf2b43263908e0e6cc3
Sharon <sharonvandergeest@live.nl>
2016-07-05 17:24:23 +0200

    figured out what I want is not possible

--- public/javascripts/productInfo.js
+++ public/javascripts/productInfo.js
@@ -1,25 +1,59 @@
 $( document ).ready(function() {
 	console.log( "ready!" );
 
-	var theproductinfo = "http://pod.opendatasoft.com/api/records/1.0/search/?dataset=pod_gtin&q=" + $("#searchquery").val() + "&facet=gpc_s_nm&facet=brand_nm&facet=owner_nm&facet=gln_nm&facet=prefix_nm&refine.prefix_nm=GS1+Netherlands&refine.gpc_s_nm=Food+-+Beverage+-+Tobacco"
+	var APIbarcode = "http://pod.opendatasoft.com/api/records/1.0/search/?dataset=pod_gtin&q=" + $("#searchquery").val() + "&facet=gpc_s_nm&facet=brand_nm&facet=owner_nm&facet=gln_nm&facet=prefix_nm&refine.prefix_nm=GS1+Netherlands&refine.gpc_s_nm=Food+-+Beverage+-+Tobacco"
+
+	var productName;
+	var productNumber;
+	var productNameNutr;
+	var productDetails;
 
 	$( '#homepage' ).show()
 	$( '#resultpage' ).hide()
 
 	$( '#clickSearch' ).click(function(event) {
 		event.preventDefault()
-		$.get( theproductinfo, function( data ) {
+		$.get( APIbarcode, function( barcodedata ) {
 			console.log($("#searchquery").val())
-			for (var i = 0; i < data.records.length; i++) {
-				console.log(data.records[i].fields.gtin_cd + " with name " + data.records[i].fields.gtin_nm)
-				$("#results").append('<li>' + data.records[i].fields.gtin_nm + '</li>')
-			};
-
+			//for loop, if gtin_nm consists, then take that result and return.
+			for( var i = 0; i < barcodedata.records.length; i++ ){
+				if(barcodedata.records[i].fields.gtin_nm != undefined && barcodedata.records[i].fields.gtin_nm !== 'unileverfoodsolutions.fr'){
+					productName = barcodedata.records[i].fields.gtin_nm
+					console.log('The product name ' + productName)
+					i = barcodedata.records.length ++
+					//return productName;
+				}
+			}
+			$("#results").append('<li><h2>productName ' + productName + '</h2></li>')
 			$( '#homepage' ).hide()
 			$( '#resultpage' ).show()
+			console.log('now the new api is coming ' + productName)
+			var APIproductNumber = "https://api.nal.usda.gov/ndb/search/?format=json&q=" + productName + "&max=1&offset=0&api_key=FFB7WsiNR7bR4HtRIWnDOtf80CocjsLMTnG12Yn3"
 
-		});
+			$.get( APIproductNumber, function( numberdata ) {
+				//first find the specific product number through API
+				console.log('productNumber name ' + numberdata.list.item[0].name)
+				productNumber = numberdata.list.item[0].ndbno
+				productNameNutr = numberdata.list.item[0].name
+				$("#results").append('<li><h4>productNameNutr  ' + productNameNutr + '</h4></li>')
 
-	})
+				var APIproductDetails = "https://api.nal.usda.gov/ndb/reports/?ndbno=" + productNumber + "&api_key=FFB7WsiNR7bR4HtRIWnDOtf80CocjsLMTnG12Yn3"
+
+				$.get( APIproductDetails, function( productdata ) {
+					//then fill in that number and get product details.
+					productDetails = productdata.report.food.nutritions
+					console.log('productDetails' + productDetails)
+					$("#results").append('<li><p> Contains per 100 gram: </p></li>')
+					for( var i = 0; i < productDetails.length; i++ ){
+						$("#results").append('<li>productDetails' + productDetails[i].name + ": " + productDetails[i].value 
+							+ " " + productDetails[i].unit + '</li>')
+					}
+				})
+
+			})
+
+});
+
+})
 });
 
